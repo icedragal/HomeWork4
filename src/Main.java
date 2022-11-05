8453a34da7453a24d58ca28135b572374e590c91
Icedragal <icedragal@gmail.com>
2022-11-05 18:38:25 +0300

    Задание 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        //Task1
-        System.out.println("Задание 1");
+        //Task1.1
+        System.out.println("Задание 1.1");
         int age = 18;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
