d52280cedd0fa0fa410855ea21bce7c67331c95c
Icedragal <icedragal@gmail.com>
2022-11-05 19:28:58 +0300

    Задание 3.1

--- src/Main.java
+++ src/Main.java
@@ -63,5 +63,18 @@ public class Main {
             System.out.println("Есть стоячие места.");
         }
 
+        //Task3.1
+        System.out.println("Задание 3.1");
+        if (age >= 2 && age <= 6){
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад.");
+        } else if (age >= 7 && age <= 18){
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу.");
+        } else if (age > 19 && age < 24){
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет.");
+        }else if (age > 24){
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу.");
+        }
+
+
     }
 }
