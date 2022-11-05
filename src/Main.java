8dcb11631def1efbdd9a2a6f3a5d7447e15ecf01
Icedragal <icedragal@gmail.com>
2022-11-05 19:14:22 +0300

    Задание 2.2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         //Task1.1
         System.out.println("Задание 1.1");
-        int age = 24;
+        int age = 17;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
         }
@@ -34,6 +34,22 @@ public class Main {
         if (people >= 60 && people <= 102) {
             System.out.println("Есть стоячие места.");
         }
+        //Task2.1
+        System.out.println("Задание 2.1");
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        } else  {
+            System.out.println("Совершеннолетие еще не наступило. Нужно немного подождать.");
+        }
 
+        //Task2.2
+        System.out.println("Задание 2.2");
+        if (age >= 7 && age <18){
+            System.out.println("Ребенок ходит в школу.");
+        } else if (age >= 18 && age < 24){
+            System.out.println("Человек уже закончил школу и может отправляться в университет.");
+        } else if (age >= 24){
+            System.out.println("Человек окончил университет и ему пора искать первую работу.");
+        }
     }
 }
